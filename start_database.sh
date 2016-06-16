7ede181c7c70735f4ddd71e2b5a63ad64b443514
panxkv <kamil.kiedrzyn@gmail.com>
2016-06-16 16:29:40 +0200

    Remove ip form start_database.sh

--- start_database.sh
+++ start_database.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-docker run -p 127.0.0.1:6666:5432 -e POSTGRES_USER=lof -e POSTGRES_PASSWORD=lof -d postgres
+docker run -p 6666:5432 -e POSTGRES_USER=lof -e POSTGRES_PASSWORD=lof -d postgres
